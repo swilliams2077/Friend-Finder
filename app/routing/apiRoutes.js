15370e3b805a9aa72a65026a44318d653c306685
Shakwan Williams <shakwanwilliamsart@gmail.com>
2019-09-20 12:53:34 -0400

    Added to survey.html infrastructure.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,9 @@
-var friendArray = require('../data/friends.js')
+var friends = require('../data/friends.js')
 
 module.exports = function(app) {
 
     app.get("/api/friends", function (req, res) {
-        res.json(friendArray)
-    })
-
-    app.post("/api/friends", function (req, res) {
-       
-        req.body.scores = req.body.scores.map((item) => {
-        
-        return parseInt(item)
+        res.json(friends)
     })
 
 };
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
 app.use(bodyParser.text({ type: 'text/html' }))
 
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT)
 });
